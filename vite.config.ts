24bc78019d9a63ef5d5833e8902dd29efefee186
SR6Glory <satratana1@gmail.com>
2026-04-13 14:27:22 +0200

    fix: remove SESSION_SECRET from environment variables in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,6 @@ export default defineConfig(({ mode }) => {
     define: {
       "process.env.API_BASE_URL": JSON.stringify(env.API_BASE_URL),
       "process.env.PLUMPI_ENDPOINT": JSON.stringify(env.PLUMPI_ENDPOINT),
-      "process.env.SESSION_SECRET": JSON.stringify(env.SESSION_SECRET),
       "process.env.NODE_ENV": JSON.stringify(mode),
     },
   };
